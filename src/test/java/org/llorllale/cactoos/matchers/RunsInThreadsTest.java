c25bd38046f4567926613260a8dc6b858466c707
George Aristy <george.aristy@gmail.com>
2019-04-21 10:20:22 -0400

    (#110) Begin replacing Assertion -> Assertion2

--- src/test/java/org/llorllale/cactoos/matchers/RunsInThreadsTest.java
+++ src/test/java/org/llorllale/cactoos/matchers/RunsInThreadsTest.java
@@ -52,16 +52,14 @@ public final class RunsInThreadsTest {
         final AtomicInteger counter = new AtomicInteger(0);
         final int threads = 20;
         final int attempts = 100;
-        new Assertion<>(
-            "must match thread-safe Func",
-            () -> new RunsInThreads<>(counter, threads),
-            new Matches<>(
-                new RepeatedFunc<>(new Safe(), attempts)
-            )
+        new Assertion2<>(
+            "matches the thread-safe Func",
+            new RunsInThreads<>(counter, threads),
+            new Matches<>(new RepeatedFunc<>(new Safe(), attempts))
         ).affirm();
-        new Assertion<>(
+        new Assertion2<>(
             "counter must be incremented by all threads",
-            counter::get,
+            counter.get(),
             new IsEqual<>(threads * attempts)
         ).affirm();
     }
@@ -75,18 +73,18 @@ public final class RunsInThreadsTest {
         final AtomicInteger counter = new AtomicInteger(0);
         final int threads = 20;
         final int attempts = 100;
-        new Assertion<>(
-            "must not match Func that is not thread-safe",
-            () -> new RunsInThreads<>(counter, threads),
+        new Assertion2<>(
+            "does not match Func that is not thread-safe",
+            new RunsInThreads<>(counter, threads),
             new IsNot<>(
                 new Matches<>(
                     new RepeatedFunc<>(new Unsafe(), attempts)
                 )
             )
         ).affirm();
-        new Assertion<>(
+        new Assertion2<>(
             "counter must not be incremented by all threads",
-            counter::get,
+            counter.get(),
             new IsNot<>(new IsEqual<>(threads * attempts))
         ).affirm();
     }
